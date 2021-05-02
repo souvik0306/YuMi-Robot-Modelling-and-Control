6401f1f9a5b9bd5a447e860b1a09816802d410a2
Souvik Datta <souvikdatta123@gmail.com>
2021-05-02 18:47:51 +0530

    Table Base

--- YuMi_Robot.m
+++ YuMi_Robot.m
@@ -1,5 +1,13 @@
 clc 
 clear all
+%loads the robot
+%the coordinates mentioned are the most suitable for
+%getting a complete look at the model
 robot = loadrobot('abbYumi', 'Gravity', [0 0 -9.81]);
 iviz = interactiveRigidBodyTree(robot);
 ax = gca;
+%builds a collision object for the robotic arm
+plane = collisionBox(1.5,1.5,0.05);
+plane.Pose = trvec2tform([0.25 0 -0.025]);
+show(plane,'Parent', ax);
+
